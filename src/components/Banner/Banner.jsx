860817576af8e3c08fdf702fb3d341edbd2480c4
Mutah3r <evil.programmer.stephen@gmail.com>
2023-04-09 15:51:33 +0600

    added banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import ReactSimplyCarousel from 'react-simply-carousel';
+import './Banner.css'
+import bannerImg1 from '../../assets/images/banner-img-1.png'
+import bannerImg2 from '../../assets/images/banner-img-2.png'
+import bannerImg3 from '../../assets/images/banner-img-3.png'
+
+const Banner = () => {
+    const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+    return (
+        <div className='banner-wrapper'>
+            <ReactSimplyCarousel
+                activeSlideIndex={activeSlideIndex}
+                onRequestChange={setActiveSlideIndex}
+                itemsToShow={1}
+                itemsToScroll={1}
+                forwardBtnProps={{
+                    show: false
+                }}
+                backwardBtnProps={{
+                    show: false
+                }}
+                responsiveProps={[
+                    { minWidth: 0, itemsToShow: 1 }
+                ]}
+                dotsNav={{
+                    show: true,
+                    itemBtnProps: {
+                        style: {
+                            height: 16,
+                            width: 16,
+                            borderRadius: "50%",
+                            border: '1px solid #ADADAD',
+                            background: '#FFFFFF',
+                            margin: '0 5px'
+                        }
+                    },
+                    activeItemBtnProps: {
+                        style: {
+                            height: 16,
+                            width: 16,
+                            borderRadius: "50%",
+                            border: 0,
+                            background: "#EDA415",
+                            margin: '0 5px'
+                        }
+                    }
+                }}
+                autoplay={true}
+                delay={1000}
+                speed={400}
+                easing="linear"
+            >
+                {/* Banner 1 */}
+                <div className="banner-slide">
+                    <div className="banner-action-container">
+                        <h1>Canon<br />Camera</h1>
+                        <div className="banner-action-btn-container">
+                            <button className="button-primary">Shop now</button>
+                            <button className="button-secondary">View more</button>
+                        </div>
+                    </div>
+                    <div className="banner-image-container">
+                        <img draggable={false} src={bannerImg1} alt="" className="banner-image" />
+                        <div className="banner-discount">
+                            <h4>only $89</h4>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Banner 2 */}
+                <div className="banner-slide">
+                    <div className="banner-action-container">
+                        <h1>Acer<br />Laptop</h1>
+                        <div className="banner-action-btn-container">
+                            <button className="button-primary">Shop now</button>
+                            <button className="button-secondary">View more</button>
+                        </div>
+                    </div>
+                    <div className="banner-image-container">
+                        <img draggable={false} src={bannerImg2} alt="" className="banner-image" />
+                        <div className="banner-discount">
+                            <h4>only $299</h4>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Banner 3 */}
+                <div className="banner-slide">
+                    <div className="banner-action-container">
+                        <h1>Cotton<br />Jacket</h1>
+                        <div className="banner-action-btn-container">
+                            <button className="button-primary">Shop now</button>
+                            <button className="button-secondary">View more</button>
+                        </div>
+                    </div>
+                    <div className="banner-image-container">
+                        <img draggable={false} src={bannerImg3} alt="" className="banner-image" />
+                        <div className="banner-discount">
+                            <h4>only $9</h4>
+                        </div>
+                    </div>
+                </div>
+                
+
+            </ReactSimplyCarousel>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import './Home.css'
 import FeaturedCategories from '../FeaturedCategories/FeaturedCategories';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     
     return (
         <>
+            <Banner />
             <FeaturedCategories />
         </>
     );
